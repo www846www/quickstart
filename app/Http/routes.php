bc189493e0573edd9459a6760ccedb2bf82f47a3
www846www <e0981298711@gmail.com>
2017-10-04 18:56:44 +0800

    tasks視圖建置完成

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,46 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+
+    /**
+     * 顯示所有任務
+     */
+    Route::get('/', function () {
+
+        return view('tasks');
+    });
+
+    /**
+     * 增加新的任務
+     */
+     Route::post('/task', function (Request $request) {
+        // 驗證輸入
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+    
+        // 建立該任務...
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+        return redirect('/');
+    
+    });
+    
+
+    /**
+     * 刪除任務
+     */
+    Route::delete('/task/{task}', function (Task $task) {
+        //
+    });
 });
